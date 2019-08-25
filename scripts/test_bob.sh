97d22150ac07d5b61d1912d9f87178ba181c61d0
Orde <orde@orde.org.uk>
2019-08-25 17:53:39 +0100

    New names and missing requirement.

--- scripts/test_bob.sh
+++ scripts/test_bob.sh
@@ -1,2 +1,2 @@
 set -ex
-python test.py --dataroot ./datasets/bob --name color_pix2pix --model colorization --gpu_ids -1
+python test.py --dataroot ./datasets/bob --name bob --model colorization --gpu_ids -1
--- scripts/train_bob.sh
+++ scripts/train_bob.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ./datasets/bob --name color_pix2pix --model colorization --gpu_ids -1 --continue_train
+python train.py --dataroot ./datasets/bob --name bob --model colorization --gpu_ids -1 --continue_train
