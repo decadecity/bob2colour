0b122fedd3458d81cec3927bbda53e4546272a81
Orde Saunders <orde@decadecity.net>
2019-08-26 17:00:09 +0100

    Better training options.

--- scripts/train_bob.sh
+++ scripts/train_bob.sh
@@ -1,3 +1,3 @@
 set -ex
 python train.py --dataroot ./datasets/bob --name bob2colour --model colorization --gpu_ids -1 \
-    --save_epoch_freq 1 --save_by_iter --display_freq 100 --update_html_freq 100
+    --save_epoch_freq 1 --display_freq 100 --save_latest_freq 1000 --update_html_freq 100  --continue_train
